2723a5260e829f1906f508540db3ae1588f7f789
mht20 <mht252028@gmail.com>
2023-03-06 17:09:30 +0000

    Return the max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,19 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max= arr[0];
+        for(int i = 0 ; i < arr.length; i++){
+            if(max < arr[i]){
+               max = arr[i];
+            
+            // }
+            // if(arr[i]> max){
+            //     max = arr[i];
+            // }
+
+        }
+        //return max;
     }
+    return max;
+}
 }
